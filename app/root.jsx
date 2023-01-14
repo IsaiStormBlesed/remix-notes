7c3278d32850306b8e2513433a41b1599aa39702
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2023-01-14 22:06:01 +0000

    home & main styles added

--- app/root.jsx
+++ app/root.jsx
@@ -7,11 +7,22 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import globalStyles from "~/styles/main.css";
+
 export const meta = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStyles,
+    },
+  ];
+};
 
 export default function App() {
   return (
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,16 @@
 import { Link } from "@remix-run/react";
 
+import homeStyles from "~/styles/home.css";
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: homeStyles,
+    },
+  ];
+};
+
 export default function Index() {
   return (
     <main id="content">
